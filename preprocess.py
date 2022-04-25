7cc882c510ad93fa74626c0a48150bd1b12de586
134130U <mbabou@aimsammi.org>
2022-04-25 08:37:27 +0000

    update with logistique feedbacck

--- preprocess.py
+++ preprocess.py
@@ -8,7 +8,6 @@ import datetime as t
 # Data preprocessing
 
 def prepar():
-
     df_old = pd.read_csv('Data/prev_data.csv')
     df_stock = pd.read_csv('Data/stock.csv')
     df_price = pd.read_csv('Data/price.csv')
@@ -19,44 +18,47 @@ def prepar():
     df_stock = df_stock[~df_stock['true_serial'].isnull()]
     df_stock.drop_duplicates(inplace=True)
 
-    df_system = df_stock[df_stock['is_main_system']==True]
+    df_system = df_stock[df_stock['is_main_system'] == True]
     df_value_age = df_system.merge(df_age, on='product_id', how='inner')
     value_cond = (df_value_age['etat'].isin(['Endommagé', 'waste', 'canceled']) | df_value_age['is_archived'] == True)
     df_value_age['valeur'] = np.where(value_cond, 0, df_value_age['cost_price'])
-    df_age_new = df_value_age[['date_at', 'mois', 'category', 'product_type', 'country', 'valeur']].groupby(
-        ['date_at', 'mois','bucket', 'product_type', 'category', 'country']).sum().reset_index()
+    df_age_new = df_value_age[['date_at', 'mois', 'category', 'product_type', 'bucket', 'country', 'valeur']].groupby(
+        ['date_at', 'mois', 'bucket', 'product_type', 'category', 'country']).sum().reset_index()
 
-    df_system.loc[:,'group'] = 'system'
+    df_system.loc[:, 'group'] = 'system'
 
-    df_compo = df_stock[df_stock['is_main_system']==False]
-    df_price.columns = ['product_type','type','Cout']
-    df_compo1 = df_compo.merge(df_price ,on='product_type',how='left')
+    df_compo = df_stock[df_stock['is_main_system'] == False]
+    df_price.columns = ['product_type', 'type', 'Cout']
+    df_compo1 = df_compo.merge(df_price, on='product_type', how='left')
     df_system_decom = df_compo1[df_compo1['Cout'].isnull()]
-    df_system_decom.loc[:,'group'] = 'principal'
+    df_system_decom.loc[:, 'group'] = 'principal'
 
-    df_system_decom = df_system_decom.merge(df_remove,on='product_type',how = 'left')
+    df_system_decom = df_system_decom.merge(df_remove, on='product_type', how='left')
     df_system_decom.head()
-    df_system_decom['cost_price'] = df_system_decom['cost_price']-df_system_decom['prix']
+    df_system_decom['cost_price'] = df_system_decom['cost_price'] - df_system_decom['prix']
     col = list(df_system.columns)
     df_system_decom = df_system_decom[col]
 
-    df_composant= df_compo1[df_compo1['Cout'].notnull()]
-    df_composant.loc[:,'group'] = 'composant'
-    df_composant.loc[:,'cost_price'] = df_composant.loc[:,'Cout']
+    df_composant = df_compo1[df_compo1['Cout'].notnull()]
+    df_composant.loc[:, 'group'] = 'composant'
+    df_composant.loc[:, 'cost_price'] = df_composant.loc[:, 'Cout']
     df_composant = df_composant[col]
-    data = pd.concat([df_system,df_system_decom,df_composant],axis=0)
+    data = pd.concat([df_system, df_system_decom, df_composant], axis=0)
 
-    value_condition = (data['etat'].isin(['Endommagé','waste','canceled']) | data['is_archived']==True)
-    loss_condition = (data['etat'].isin(['Endommagé','waste','canceled']) | ~data['stock_name'].str.lower().str.startswith('zzz'))
-    reimburse_condition = (data['etat'].isin(['Endommagé','waste','canceled']) | ~data['stock_name'].str.lower().str.startswith('yyy'))
+    value_condition = (data['etat'].isin(['Endommagé', 'waste', 'canceled']) | data['is_archived'] == True)
+    loss_condition = (
+                data['etat'].isin(['Endommagé', 'waste', 'canceled']) | ~data['stock_name'].str.lower().str.startswith(
+            'zzz'))
+    reimburse_condition = (
+                data['etat'].isin(['Endommagé', 'waste', 'canceled']) | ~data['stock_name'].str.lower().str.startswith(
+            'yyy'))
 
+    data['valeur'] = np.where(value_condition, 0, data['cost_price'])
+    data['loss'] = np.where(loss_condition, 0, data['cost_price'])
+    data['reimburse'] = np.where(reimburse_condition, 0, data['cost_price'])
 
-    data['valeur'] = np.where(value_condition,0,data['cost_price'])
-    data['loss'] = np.where(loss_condition,0,data['cost_price'])
-    data['reimburse'] = np.where(reimburse_condition,0,data['cost_price'])
-    
-    df_new = data[['date_at','mois','category','product_type','country','valeur','loss','reimburse']].groupby(['date_at','mois','product_type','category','country']).sum().reset_index()
-
+    df_new = data[['date_at', 'mois', 'category', 'product_type', 'country', 'valeur', 'loss', 'reimburse']].groupby(
+        ['date_at', 'mois', 'product_type', 'category', 'country']).sum().reset_index()
     finale = pd.concat([df_old,df_new],axis=0)
     finale_age = pd.concat([df_age_old, df_age_new], axis=0)
     if t.date.today().day ==1:
